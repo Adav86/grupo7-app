aa9fd902b2f6dda9dc6ba5aba85fd829c0bbd590
Monica Alejandra Gallardo <adav86@hotmail.com>
2023-06-12 20:48:00 -0300

    Creacion de Carrusel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import {  Carrusel} from "./Componentes/Carrusel";
 import { Inicio } from './Paginas/Inicio/Inicio';
 
 function App() {
   return (
     <BrowserRouter>
+      <Carrusel/>
       <Routes>
         <Route path='/' element={<Inicio/>}></Route>
       </Routes>
