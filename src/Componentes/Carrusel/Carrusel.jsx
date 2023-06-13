f8660fec9b1e4c004b245b9b23b12c6afb9517eb
Monica Alejandra Gallardo <adav86@hotmail.com>
2023-06-13 15:22:14 -0300

    Creacion de carrusel por boostrap

--- src/Componentes/Carrusel/Carrusel.jsx
+++ src/Componentes/Carrusel/Carrusel.jsx
@@ -1,47 +1,29 @@
 import "./Carrusel.css"
-import second from 'first'
-import "./Carrusel.css";import Carousel from 'react-bootstrap/Carousel';
+import React from "react"
 
-function Carrusel() {
-  return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+export const Carrusel = () => {
+    return (
+
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="..." class="d-block w-100" alt="..."></img>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="..." class="d-block w-100" alt="..."></img>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    )
 }
-
-export default Carrusel;
--- src/Paginas/Inicio/Inicio.jsx
+++ src/Paginas/Inicio/Inicio.jsx
@@ -1,10 +1,14 @@
 import { Miniatura } from "../../Componentes/Miniatura/Miniatura";
 import "./Inicio.css";
 import { objetoJuego } from "../../Componentes/Miniatura/objetoJuego";
+import {Carrusel} from "../../Componentes/Carrusel/Carrusel";
 
 export const Inicio = ()=>{
     return (
+       <div>
+        <Carrusel/>
         <Miniatura objetoJuego={objetoJuego}/>
+        </div> 
     )
 
 }
