8863ea44cc9434e5199c48a547c2fceda17f0e15
jesuseduaardo <jesuseduaardo@gmail.com>
2023-06-23 21:51:53 -0300

    Construccion de la pagina de generos

--- src/Paginas/Generos/Generos.jsx
+++ src/Paginas/Generos/Generos.jsx
@@ -1,7 +1,37 @@
-import { React } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { LogoCarga } from "../../Componentes/Logo/LogoCarga";
+import { Miniatura } from "../../Componentes/Miniatura/Miniatura";
+import { juegosApi } from "../../Utilidades/api";
 import "./Generos.css"
 
 export const Generos = () => {
-  return <LogoCarga />;
+  /*  /genres */
+  //return <LogoCarga />
+  const [generos, setGeneros] = useState(null);
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    if (generos === null) {
+      const resultado = juegosApi('genres');
+      if (resultado) {
+        setGeneros(resultado);
+        setCargando(false);
+      }
+    }
+  }, [])
+
+  if (cargando) {
+    return (<LogoCarga />)
+  } else {
+    return ( ''
+      /* generos && generos.length > 0 ?
+        generos.map((genero, key) => (
+          <Miniatura key={key} objetoJuego={genero} />
+        ))
+        : '' */
+    )
+  }
+
+
 };
